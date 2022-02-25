58d4793d1f9b83e592294ec649dcf0a32f1df904
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-02-25 21:40:25 +0530

    Side Drawer basic code added.

--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
+import './NavLinks.css';
+
+function NavLinks(props) {
+    return (
+        <ul className="nav-links-list">
+            <li>
+                <NavLink to="/" exact>ALL USERS</NavLink>
+            </li>
+            <li>
+                <NavLink to="/u01/quotes">MY QUOTES</NavLink>
+            </li>
+            <li>
+                <NavLink to="/quotes/new">ADD QUOTE</NavLink>
+            </li>
+            <li>
+                <NavLink to="/auth">AUTHENTICATE</NavLink>
+            </li>
+        </ul>
+    );
+}
+
+export default NavLinks;
--- frontend/src/shared/components/Navigation/Navigation.js
+++ frontend/src/shared/components/Navigation/Navigation.js
@@ -2,22 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import NavHeader from './NavHeader';
+import SideDrawer from './SideDrawer';
+import NavLinks from './NavLinks';
 
 import './Navigation.css';
 
 function Navigation(props) {
     return (
-        <NavHeader>
-            <button className="navigation__menu-btn">
-                <span />
-                <span />
-                <span />
-            </button>
-            <h1 className="navigation__title">
-                <Link to="/">Your Quotes</Link>
-            </h1>
-            <nav>Nav Links</nav>
-        </NavHeader>
+        <React.Fragment>
+            <SideDrawer>
+                <nav className="navigation__drawer-nav">
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
+            <NavHeader>
+                <button className="navigation__menu-btn">
+                    <span />
+                    <span />
+                    <span />
+                </button>
+                <h1 className="navigation__title">
+                    <Link to="/">Your Quotes</Link>
+                </h1>
+                <nav className="navigation__header-nav">
+                    <NavLinks />
+                </nav>
+            </NavHeader>
+        </React.Fragment>
     );
 }
 
