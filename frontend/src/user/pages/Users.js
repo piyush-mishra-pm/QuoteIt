8ecac8f2c2cef58b6b7da8d97ceccbfb4d3e9d07
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-03-12 17:44:33 +0530

    custom HTTP Hook used to GetAllUsers.

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -3,40 +3,22 @@ import React, {useEffect, useState} from 'react';
 import UserList from '../components/UserList';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/ErrorModal';
+import useHttpClient from '../../shared/hooks/http-hook';
 
 const Users = () => {
-
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const {isLoading, sendRequest, error, clearErrorHandler} = useHttpClient();
   const [fetchedUsers, setFetchedUsers] = useState();
 
   useEffect(() =>{
     // IIFE
     (async ()=> {
       try{
-        setIsLoading(true);
-
-        const response = await fetch('http://localhost:4000/api/v1/users');
-        const data = await response.json();
-
-        if(!response.ok){
-          throw new Error(data.message);
-        }
-
+        const data = await sendRequest('http://localhost:4000/api/v1/users');
         setFetchedUsers(data.users);
-
-      }catch(err){
-        setError(err.message);
-      }
-
-      setIsLoading(false);
+      }catch(err){}
     })();
 
-  },[]);
-
-  const clearErrorHandler = () => {
-    setError(null);
-  }
+  },[sendRequest]); // sendRequest uses a useCallback, so will not lead to inifinite loop here.
 
   return (
       <React.Fragment>
